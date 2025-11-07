040ef1ffa4b54989cfcefb6fa66a5e76f6b74749
maxtong2023 <maxtong2027@u.northwestern.edu>
2025-11-06 22:05:44 -0600

    navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import "./style.css";
+export default function Navbar() {
+    return (
+            <nav className="nav">
+                <div className="title">Todo List</div>
+                <div className="date"> {new Date().toLocaleDateString()}</div> 
+            </nav>
+    )
+}
